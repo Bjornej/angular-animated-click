34248c105c01c17e626e9a1a2ba3b056db00ebf7
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-05-18 18:38:25 +0200

    completed animation

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
 	*/
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -40,6 +41,13 @@ module.exports = function(grunt) {
 					dest:   []
 				}
 			},
+            sass:{
+                compile: {
+                files: {
+                    'dist/animated-click.css': 'animated-click.scss'
+                }
+            }
+            },
 			jshint: {
 				options: {
 					//force:          true,
--- animated-click.js
+++ animated-click.js
@@ -1,5 +1,67 @@
 angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
     "use strict";
+
+    var startSpinner = function (element) {
+        var height = element.offsetHeight,
+                     spinnerColor;
+
+        angular.element(element).addClass("animated-button");
+
+        if (height === 0) {
+            // We may have an element that is not visible so
+            // we attempt to get the height in a different way
+            height = parseFloat(window.getComputedStyle(element).height);
+        }
+
+        // If the button is tall we can afford some padding
+        if (height > 32) {
+            height *= 0.8;
+        }
+
+        if (!element.querySelector('.animated-label')) {
+            element.innerHTML = '<span class="animated-label">' + element.innerHTML + '</span>';
+        }
+
+        // The spinner component
+        var spinner;
+
+        // Wrapper element for the spinner
+        var spinnerWrapper = document.createElement('span');
+        spinnerWrapper.className = 'animated-spinner';
+        element.appendChild(spinnerWrapper);
+
+
+        element.setAttribute('disabled', '');
+        element.setAttribute('data-loading', '');
+        var lines = 12,
+            radius = height * 0.2,
+            length = radius * 0.6,
+            width = radius < 7 ? 2 : 3;
+
+
+        spinner = new Spinner({
+            color: '#fff',
+            lines: lines,
+            radius: radius,
+            length: length,
+            width: width,
+            zIndex: 'auto',
+            top: 'auto',
+            left: 'auto',
+            className: ''
+        });
+
+        spinner.spin(element.querySelector('.animated-spinner'));
+
+    };
+
+    var stopSpinner = function (element) {
+        element.removeAttribute('disabled');
+        element.removeAttribute('data-loading');
+        angular.element(element).removeClass("animated-button");
+        angular.element(element.querySelector('.animated-spinner')).remove();
+    };
+
     return {
         restrict: "A",
         controller: ['$scope', function ($scope) {
@@ -9,6 +71,7 @@ angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
         compile: function ($element, attr) {
             var fn = $parse(attr.ngSingleClick);
             return function (scope, element, attr, controller) {
+
                 element.on("click", function (event) {
 
                     // if function is currently running don't execute it again
@@ -18,9 +81,11 @@ angular.module("ng").directive("ngSingleClick", ["$parse", function ($parse) {
                             // if invoked function returns a promise wait for it's completion
                             var result = fn(scope, { $event: event });
                             if (result.finally !== undefined) {
+                                startSpinner(element[0]);
                                 controller.running = true;
                                 result.finally(function () {
                                     controller.running = false;
+                                    stopSpinner(element[0]);
                                 });
                             }
                         });
